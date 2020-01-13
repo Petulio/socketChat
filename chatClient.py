e46063c174ade57fbeb84b442f89e2c56d39d0e5
Stefan Leiva <stefan.leiva@gmail.com>
2020-01-13 16:28:24 +0100

    Adds changes to client

--- chatClient.py
+++ chatClient.py
@@ -1,30 +1,28 @@
 import socket
+import sys
 import threading
 # 172.16.117.71
 # 172.16.117.80
 # 172.16.118.44
 # grodvidar.xyz 1234
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('172.16.117.80', 8081))
+client.connect(("172.16.117.80", 8080))
 
-def listen(lock):
+threading1 = threading.Thread(target=recvMess)
+threading1.daemon = True
+threading1.start()
+
+def recvMess():
     while True:
         from_server = client.recv(1024)
-        if from_server != None:
-            print ('From server: ', from_server.decode('utf-8'))
-            from_server = None
-
-lock = threading.Lock()
-p = threading.Thread(target=listen, args=(lock,), daemon=True)
-p.start()
+        print("Recieved: ", from_server)
 
 while True:
-
     chatInput = input("Enter message: ")
     if chatInput != "":
-        client.send(bytes(chatInput, 'utf-8'))
-        chatInput = ""
+        client.sendall(bytes(chatInput, 'utf-8'))
+        chatInput = None
     elif chatInput == "quit":
         break
 
-client.close()
+#  client.close()
